0fb3e667daeed43649352dc5b981a5813a3785ce
Louise <louisepeiffer@hotmail.com>
2021-01-26 16:18:21 +0100

    exos finis

---
+++ main.js
@@ -0,0 +1,13 @@
+// 1
+let tart = document.getElementsByClassName('tartine')[0]
+let textTart = tart.textContent
+console.log(textTart);
+
+// 2 
+let toastie = document.getElementById('toast')
+let textToastie = toastie.textContent
+console.log(textToastie);
+
+// 3
+let tart2 = document.getElementsByClassName('tartine')[1]
+console.log(tart2);
